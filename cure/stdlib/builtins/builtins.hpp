a34f68ad8f020de5bd6560e188935c8709e18f8f
LemurGamingYT <103781879+LemurGamingYT@users.noreply.github.com>
2025-08-05 12:27:35 +0100

    Range function and operator overloads

--- cure/stdlib/builtins/builtins.hpp
+++ cure/stdlib/builtins/builtins.hpp
@@ -219,6 +219,30 @@ bool float_gt_float(float a, float b) { return a > b; }
 bool float_lte_float(float a, float b) { return a <= b; }
 bool float_gte_float(float a, float b) { return a >= b; }
 
+float float_add_int(float a, int b) { return float_add_float(a, (float)b); }
+float float_sub_int(float a, int b) { return float_sub_float(a, (float)b); }
+float float_mul_int(float a, int b) { return float_mul_float(a, (float)b); }
+float float_div_int(float a, int b) { return float_div_float(a, (float)b); }
+float float_mod_int(float a, int b) { return float_mod_float(a, (float)b); }
+bool float_eq_int(float a, int b) { return float_eq_float(a, (float)b); }
+bool float_neq_int(float a, int b) { return float_neq_float(a, (float)b); }
+bool float_lt_int(float a, int b) { return float_lt_float(a, (float)b); }
+bool float_gt_int(float a, int b) { return float_gt_float(a, (float)b); }
+bool float_lte_int(float a, int b) { return float_lte_float(a, (float)b); }
+bool float_gte_int(float a, int b) { return float_gte_float(a, (float)b); }
+
+float int_add_float(int a, float b) { return float_add_float((float)a, b); }
+float int_sub_float(int a, float b) { return float_sub_float((float)a, b); }
+float int_mul_float(int a, float b) { return float_mul_float((float)a, b); }
+float int_div_float(int a, float b) { return float_div_float((float)a, b); }
+float int_mod_float(int a, float b) { return float_mod_float((float)a, b); }
+bool int_eq_float(int a, float b) { return float_eq_float((float)a, b); }
+bool int_neq_float(int a, float b) { return float_neq_float((float)a, b); }
+bool int_lt_float(int a, float b) { return float_lt_float((float)a, b); }
+bool int_gt_float(int a, float b) { return float_gt_float((float)a, b); }
+bool int_lte_float(int a, float b) { return float_lte_float((float)a, b); }
+bool int_gte_float(int a, float b) { return float_gte_float((float)a, b); }
+
 string string_add_string(const string& a, const string& b) { return a + b; }
 bool string_eq_string(const string& a, const string& b) { return a == b; }
 bool string_neq_string(const string& a, const string& b) { return a != b; }
@@ -257,3 +281,21 @@ nil assert(bool b, const string& msg) {
 }
 
 nil assert(bool b) { return assert(b, "assertion failed"); }
+
+array<int> range(int start, int end) {
+    array<int> arr;
+    for (int i = start; i < end; i++) arr.add(i);
+    return arr;
+}
+
+array<int> range(int end) {
+    array<int> arr;
+    for (int i = 0; i < end; i++) arr.add(i);
+    return arr;
+}
+
+array<int> range(int start, int end, int step) {
+    array<int> arr;
+    for (int i = start; i < end; i += step) arr.add(i);
+    return arr;
+}
